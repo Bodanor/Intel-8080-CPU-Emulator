8e61f286739a5c13a2f54747bc564d4eccf6e3e5
Bodanor <christos.papadopoulos@student.hepl.be>
2022-07-31 14:51:38 +0200

    ADD REGISTER PAIRS ADDRESSABLE WAY

--- 8080_cpu.h
+++ 8080_cpu.h
@@ -19,12 +19,35 @@ typedef struct flags_8080
 typedef struct registers_8080
 {
     uint8_t a;
-    uint8_t b;
-    uint8_t c;
-    uint8_t d;
-    uint8_t e;
-    uint8_t h;
-    uint8_t l;
+    union
+    {
+        struct
+        {
+            uint8_t c;
+            uint8_t b;
+        };
+        uint16_t bc;
+    };
+    
+    union
+    {
+        struct
+        {
+            uint8_t e;
+            uint8_t d;
+        };
+        uint16_t de;
+    };
+    union
+    {
+        struct
+        {
+            uint8_t l;
+            uint8_t h;
+        };
+        uint16_t hl;
+    };
+
     uint16_t sp;
     uint16_t pc;
     uint8_t *memory;
